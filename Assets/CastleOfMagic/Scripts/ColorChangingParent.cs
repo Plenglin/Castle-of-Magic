42077bb82c0841a2aca3d896c09de7a42c2596d3
19YuM76 <19YuM76@smuhsd.org>
2018-08-29 14:29:32 -0700

    idk is it workingo r not asfdsagsadkljfgsadfgsadg

---
+++ Assets/CastleOfMagic/Scripts/ColorChangingParent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ColorChangingParent : NetworkBehaviour {
+	[SyncVar]
+	public Color color;
+
+	public void OnColorChanged(Color nc) {
+		GetComponent<Image>().color = nc;
+	}
+}
