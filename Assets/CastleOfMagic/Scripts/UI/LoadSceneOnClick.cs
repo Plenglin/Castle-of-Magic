6a6a5dac99af1b0e70e15c593dc21ad6c95b7f6e
Max <maximumpwnguin@gmail.com>
2018-09-05 13:16:51 -0700

    Make menu an actual menu

---
+++ Assets/CastleOfMagic/Scripts/UI/LoadSceneOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneOnClick: MonoBehaviour {
+
+    public string scene;
+
+    private UnityAction action;
+
+    private void Start() {
+        action += DoLoadScene;
+        GetComponent<Button>().onClick.AddListener(action);
+    }
+
+    public void DoLoadScene() {
+        SceneManager.LoadScene(scene);
+    }
+}
