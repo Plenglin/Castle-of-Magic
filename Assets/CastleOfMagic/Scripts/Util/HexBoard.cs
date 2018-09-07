3b3f18713cd34c02812255f51fd725324082d160
Jerry <jerry21yan@gmail.com>
2018-09-07 11:47:54 -0700

    start of hex board

---
+++ Assets/CastleOfMagic/Scripts/Util/HexBoard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace CastleMagic.Util
+{
+    public class HexBoard
+    {
+        BitArray[] coordinates;
+
+        public HexBoard()
+        {
+            
+        }
+    }
+}
