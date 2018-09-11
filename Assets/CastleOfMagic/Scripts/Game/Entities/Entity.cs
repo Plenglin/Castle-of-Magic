ac30318b8d76143d5adec2b1313b9c5936a06c70
Jerry <jerry21yan@gmail.com>
2018-09-11 14:16:39 -0700

    i am crate entity!

---
+++ Assets/CastleOfMagic/Scripts/Game/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CastleMagic.Util.Hex;
+using UnityEngine.Networking;
+
+namespace CastleMagic.Game.Entites
+{
+    /// <summary>
+    /// Represents any object that has a position on the board and cannot be passed through.
+    /// </summary>
+    public abstract class Entity : NetworkBehaviour
+    {
+        public HexCoord position;
+
+        protected int maxEnergy;
+        protected int energy;
+        protected int maxHealth;
+        protected int health;
+        protected bool invunerable;
+    }
+}
