1657ffd222076a932d36bc2a9c1f6a49820f1286
Max <maximumpwnguin@gmail.com>
2018-09-11 14:13:52 -0700

    cool and good tirangles are good

--- Assets/CastleOfMagic/Scripts/Util/Hex/HexMesh.cs
+++ Assets/CastleOfMagic/Scripts/Util/Hex/HexMesh.cs
@@ -7,29 +7,36 @@ namespace CastleMagic.Util.Hex {
     public class HexMesh : MonoBehaviour {
 
         private Vector3[] vertices;
+        private int[] triangles;
         private Mesh mesh;
 
         public int width = 10;
         public int height = 10;
 
-        private int offset;
+        private int vertRowOffset;
 
         private void Awake() {
             GenerateVertices();
+            GenerateTriangles();
+
+            mesh = new Mesh();
+            mesh.name = "Procedural Hex Grid";
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            GetComponent<MeshFilter>().mesh = mesh;
         }
 
         private void GenerateVertices() {
-            offset = width + 1;
+            vertRowOffset = width + 1;
             vertices = new Vector3[width * height * 4];
             for (int y = 0; y < height / 2 + 1; y++) {
                 int y4 = 4 * y;
-                int[] indexOffsets = new int[] { offset * y4, offset * (y4 + 1), offset * (y4 + 2), offset * (y4 + 3) };
+                int[] indexOffsets = new int[] { vertRowOffset * y4, vertRowOffset * (y4 + 1), vertRowOffset * (y4 + 2), vertRowOffset * (y4 + 3) };
                 float[] xOffsets = new float[] { 0, 0.5f, 0.5f, 1 };
                 float[] yOffsets = new float[] { 0, HexCoord.HEX_HEIGHT / 4, HexCoord.HEX_HEIGHT * 3 / 4, HexCoord.HEX_HEIGHT };
                 for (int i = 0; i < 4; i++) {
                     for (int x = 0; x <= width; x++) {
                         int index = indexOffsets[i] + x;
-                        Debug.Log(index);
                         vertices[index] = new Vector3(
                             y + x + xOffsets[i],
                             1.5f * HexCoord.HEX_HEIGHT * y + yOffsets[i],
@@ -41,7 +48,37 @@ namespace CastleMagic.Util.Hex {
         }
 
         private void GenerateTriangles() {
+            triangles = new int[12 * width * height];
+            for (int y=0; y < height; y++) {
+                int rowOffset = y * width;
+                for (int x=0; x < width; x++) {
+                    int triIndexOffset = 12 * (rowOffset + x);
+                    int yOffset0 = (2 * y) * vertRowOffset;
+                    int yOffset1 = (2 * y + 1) * vertRowOffset;
+                    int yOffset2 = (2 * y + 2) * vertRowOffset;
+                    int yOffset3 = (2 * y + 3) * vertRowOffset;
 
+                    triangles[triIndexOffset + 0] = yOffset0 + x + 1;
+                    triangles[triIndexOffset + 1] = yOffset1 + x + 1;
+                    triangles[triIndexOffset + 2] = yOffset1 + x;
+
+                    triangles[triIndexOffset + 3] = yOffset1 + x;
+                    triangles[triIndexOffset + 4] = yOffset1 + x + 1;
+                    triangles[triIndexOffset + 5] = yOffset2 + x;
+
+                    triangles[triIndexOffset + 6] = yOffset2 + x + 1;
+                    triangles[triIndexOffset + 7] = yOffset2 + x;
+                    triangles[triIndexOffset + 8] = yOffset1 + x + 1;
+
+                    triangles[triIndexOffset + 9] = yOffset3 + x;
+                    triangles[triIndexOffset + 10] = yOffset2 + x;
+                    triangles[triIndexOffset + 11] = yOffset2 + x + 1;
+
+                }
+            }
+            /*triangles[0] = 1;
+            triangles[1] = 4;
+            triangles[2] = 5;*/
         }
 
         // Update is called once per frame
