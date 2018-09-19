50d9b33a26a8199ca83af339897fc86f792c72e9
Max <maximumpwnguin@gmail.com>
2018-09-19 14:25:56 -0700

    oof

--- Assets/CastleOfMagic/Scripts/Game/Entities/EntityCrate.cs
+++ Assets/CastleOfMagic/Scripts/Game/Entities/EntityCrate.cs
@@ -8,7 +8,7 @@ namespace CastleMagic.Game.Entites {
 
         // Use this for initialization
         void Start() {
-            entity.OnPositionChanged(HexCoord.CreateXY(2, 5));
+            //entity.OnPositionChanged(HexCoord.CreateXY(2, 5));
         }
 
         // Update is called once per frame
