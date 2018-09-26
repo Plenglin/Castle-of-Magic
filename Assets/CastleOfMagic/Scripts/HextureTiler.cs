cd39c7a04e1ff4e034e13b89ccc10f04ceae3e2a
Max <maximumpwnguin@gmail.com>
2018-09-26 14:21:30 -0700

    yayeeeethaw

---
+++ Assets/CastleOfMagic/Scripts/HextureTiler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class HextureTiler : MonoBehaviour {
+
+    public float textureWidth;
+    public float textureHeight;
+
+    private Material mat;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 scale = 10 * transform.localScale;
+        mat.SetTextureScale("_MainTex", new Vector2(scale.x / textureWidth, scale.y / textureHeight));
+    }
+}
--- Assets/CastleOfMagic/Scripts/Util/Hex/HexPlane.cs
+++ Assets/CastleOfMagic/Scripts/Util/Hex/HexPlane.cs
@@ -8,6 +8,8 @@ namespace CastleMagic.Util.Hex {
         private Plane plane = new Plane(Vector3.down, 0);
         public float scale = 1.0f;
 
+        public bool drawHexPoints = true;
+
         public HexCoord? RaycastToHex(Ray ray) {
             var relativeRay = new Ray(transform.InverseTransformPoint(ray.origin), transform.InverseTransformDirection(ray.direction));
             Debug.DrawRay(ray.origin, 10 * ray.direction, Color.white);
@@ -50,14 +52,16 @@ namespace CastleMagic.Util.Hex {
             Gizmos.DrawLine(transform.position, length * HexToPlanePosition(HexCoord.UP_R));
             Gizmos.color = Color.white;
             Gizmos.DrawWireSphere(transform.position, scale);
+            if (drawHexPoints) {
+                for (int x = 0; x < 30; x++) {
+                    for (int y = 0; y < 30; y++) {
+                        Gizmos.DrawSphere(HexToPlanePosition(HexCoord.CreateXY(x, y)), 0.1f * scale);
+                    }
+                }
+            }
         }
 
         private void OnDrawGizmosSelected() {
-            for (int x=0; x < 10; x++) {
-                for (int y=0; y < 10; y++) {
-                    Gizmos.DrawSphere(HexToPlanePosition(HexCoord.CreateXY(x, y)), 0.1f * scale);
-                }
-            }
         }
 
     }
