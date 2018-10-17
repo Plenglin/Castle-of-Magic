bc444e860f308b3e2a337d96e5768a44356e19c7
Jerry <jerry21yan@gmail.com>
2018-10-17 14:04:07 -0700

    idk what i do

---
+++ Assets/CastleOfMagic/Scripts/Game/GameInfo/PlayerAcrtions/ActionMove.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CastleMagic.Util.Hex;
+
+namespace CastleMagic.Game.GameInfo.PlayerAcrtions
+{
+    public class ActionMove : TurnAction
+    {
+        private HexCoord from;
+        private HexCoord to;
+        private PlayerController player;
+
+        public ActionMove(PlayerController player, HexCoord from, HexCoord to) {
+            this.from = from;
+            this.to = to;
+            this.player = player;
+        }
+
+        public override void ExecuteAction()
+        {
+            player.CmdMoveEntity(player.player.GetInstanceID(), to);
+        }
+    }
+}
