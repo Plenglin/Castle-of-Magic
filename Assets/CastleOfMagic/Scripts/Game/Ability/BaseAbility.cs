014360209901087d59414315114e45d21759564a
Max <maximumpwnguin@gmail.com>
2018-11-13 14:49:35 -0800

    targeting stuff

---
+++ Assets/CastleOfMagic/Scripts/Game/Ability/BaseAbility.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CastleMagic.Game.Ability {
+
+    [CreateAssetMenu(fileName = "Ability", menuName = "Castle of Magic/New Ability", order = 1)]
+    public class BaseAbility : ScriptableObject {
+        public string displayName = "Ability";
+        public int energy = 0;
+        public int regeneration = 1;
+        public int charges = 1;
+    }
+
+}
