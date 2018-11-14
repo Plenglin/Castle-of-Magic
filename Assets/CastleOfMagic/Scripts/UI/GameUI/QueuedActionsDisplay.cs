4a713f95775d4bb77d9d0b01cd21dca19944947e
Jerry <jerry21yan@gmail.com>
2018-11-14 14:15:21 -0800

    ghvbhjn

--- Assets/CastleOfMagic/Scripts/UI/GameUI/QueuedActionsDisplay.cs
+++ Assets/CastleOfMagic/Scripts/UI/GameUI/QueuedActionsDisplay.cs
@@ -1,20 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using CastleMagic.Game.GameInfo.PlayerActions;
+using System.Collections.Generic;
+using System;
 
 namespace CastleMagic.UI.GameUI {
     public class QueuedActionsDisplay : MonoBehaviour {
 
-        private Text text;
+        public List<TurnAction> actions;
+        public Text text;
 
         // Use this for initialization
         void Start() {
+            actions = new List<TurnAction>();
             text = GetComponent<Text>();
         }
 
         // Update is called once per frame
         void Update() {
 
+        }
+
+        public void AddAction(TurnAction action) {
+            actions.Add(action);
+            OnActionListChange();
+        }
+
+        public void UndoAction() {
+            actions.RemoveAt(actions.Count - 1);
+            OnActionListChange();
+        } 
+
+        void OnActionListChange() {
+            text.text = "";
+            foreach(TurnAction ta in actions) {
+                text.text += ta + Environment.NewLine;
+            }
+        }
+
+        public void OnNewTurn() {
+            actions.Clear();
+            OnActionListChange();
         }
     }
 }
